039fedea884d46c6e4a98f23e22973feaa586371
Brunno Quadrado <bbertoninuti@gmail.com>
2024-09-07 13:21:53 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department d = new Department(1, "books");
 		
 		Seller s = new Seller(1, "kanye", "kanye@gmail.com", new Date(), 3000.0, d);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(s);
 		System.out.println(d);

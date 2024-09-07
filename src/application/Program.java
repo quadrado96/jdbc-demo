34d003edf2f46112ef1bbfd79f4e5e5a1e25a7a0
Brunno Quadrado <bbertoninuti@gmail.com>
2024-09-07 13:07:30 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department d = new Department(1, "books");
 		
+		Seller s = new Seller(1, "kanye", "kanye@gmail.com", new Date(), 3000.0, d);
+		
+		System.out.println(s);
 		System.out.println(d);
 	}
 

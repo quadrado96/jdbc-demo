dab8da2c38b7090bbcf7b1ad7643a5bed6e885d9
Brunno Quadrado <bbertoninuti@gmail.com>
2024-09-07 14:04:34 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,10 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		System.out.println("---------- TEST seller findById -------");
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 		}

81a55af41c73e2b6e5ade15bedf9e5d2a17b41ce
Brunno Quadrado <bbertoninuti@gmail.com>
2024-09-07 16:11:32 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,7 +40,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new Id= " + newSeller.getId());
-		
+	
+		System.out.println("\n---------- TEST 5 seller insert -------");
+		seller = sellerDao.findById(1);
+		seller.setName("Bruce Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update done!");
 		}
+	
+	
 
 }
